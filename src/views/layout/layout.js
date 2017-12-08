70067ace4f78b5c691874691d2f9b5a08ecc9b9e
浮尘 <757213882@qq.com>
2017-12-08 18:40:12 +0800

    添加layout布局页，配置首页，贫道页，文章页路由

---
+++ src/views/layout/layout.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react"
+import { Switch, Route } from "react-router-dom"
+
+import "./laout.scss"
+
+import Home from "@/views/home/home"
+import Channel from "@/views/channel/channel"
+import Article from "@/views/article/article"
+
+import AppFooter from "@/components/app-footer/app-footer"
+
+export default class Layout extends Component {
+	render () {
+		return (
+			<div className="main">
+				<Switch>
+					<Route exact path="/" component={ Home }></Route>
+					<Route path="/channel/:id" component={ Channel }></Route>
+					<Route path="/article/:id" component={ Article }></Route>
+				</Switch>
+				<AppFooter />
+			</div>
+		)
+	}
+}
